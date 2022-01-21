2e6d50d25925e0683002926b37f39778b04b8fda
Shuvo-Roy <shuvoroysp@gmail.com>
2022-01-21 21:10:57 +0600

    add html and create a js file

---
+++ script.js
@@ -0,0 +1,5 @@
+const loginBtn=  document.getElementById("enter");
+loginBtn.addEventListener("click", function (){
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display= "none";
+})
