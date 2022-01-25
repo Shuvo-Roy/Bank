949b8164cc77658f741b443640d88afbac5c6009
Shuvo-Roy <shuvoroysp@gmail.com>
2022-01-25 20:55:54 +0600

    add some functionality and structure

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+//event for login
 const loginBtn=  document.getElementById("enter");
 loginBtn.addEventListener("click", function (){
     const loginArea = document.getElementById("login-area");
     loginArea.style.display= "none";
+    const transcationArea=document.getElementById("transc");
+    transcationArea.style.display="block";
+})
+
+//deposit event
+
+const depositBtn=document.getElementById("depo");
+depositBtn.addEventListener("click",function(){
+ const depoAmount=document.getElementById("depositAmount").value;
+ const depoNumber = parseFloat(depoAmount);
+
+ const currentdeposit= document.getElementById("currentDepo").innerText;
+ const currentdepositNumber= parseFloat(currentdeposit);
+ const totalDeposit = depoNumber + currentdepositNumber;
+ document.getElementById("currentDepo").innerText=totalDeposit;
+ document.getElementById("depositAmount").value="";
+})
+
+
+
+
+//withdraw event
+
+const WithdrawBtn=document.getElementById("withdo");
+WithdrawBtn.addEventListener("click",function(){
+ console.log("clicked");
 })
